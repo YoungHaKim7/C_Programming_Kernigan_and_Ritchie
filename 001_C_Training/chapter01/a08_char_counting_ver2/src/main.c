d5e76e23b6c3c6d157ed0e76321531e3bc8be2b6
GYoung <ytok1108@aol.com>
2025-09-20 02:27:21 +0900

    Character Counting ver2

---
+++ 001_C_Training/chapter01/a08_char_counting_ver2/src/main.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int main(void)
+{
+    printf("Type some text (Ctrl+D to end):\n");
+
+    double nc;
+
+    for (nc = 0; getchar() != EOF; ++nc) {
+        printf("%.0f\n", nc);
+    }
+
+    printf("\nDone!\n");
+
+    return 0;
+}
