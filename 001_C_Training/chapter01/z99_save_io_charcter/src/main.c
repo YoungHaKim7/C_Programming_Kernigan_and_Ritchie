a2ff057fd23b0a1307cc6cc17f76e9df3966c483
GYoung <ytok1108@aol.com>
2025-09-22 01:08:54 +0900

    fmt + IO + save file

--- 001_C_Training/chapter01/z99_save_io_charcter/src/main.c
+++ 001_C_Training/chapter01/z99_save_io_charcter/src/main.c
@@ -4,7 +4,7 @@ int main(void)
 {
     printf("Type some text (Ctrl+D to end):\n");
 
-    FILE *out = fopen("output.txt", "w"); // open file for writing
+    FILE* out = fopen("output.txt", "w"); // open file for writing
     if (!out) {
         perror("output.txt");
         return 1;
@@ -13,7 +13,7 @@ int main(void)
     int ch;
 
     while ((ch = getchar()) != EOF) {
-        fputc(ch, out);   // write the character itself
+        fputc(ch, out); // write the character itself
     }
 
     fclose(out);
