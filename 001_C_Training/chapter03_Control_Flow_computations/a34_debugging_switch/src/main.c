a1f2e63bc154785683aca5c45d93e462409107aa
GYoung <ytok1108@aol.com>
2025-09-28 21:26:13 +0900

    fmt + 3.4 Switch  디버깅으로 자세히 보자

--- 001_C_Training/chapter03_Control_Flow_computations/a34_debugging_switch/src/main.c
+++ 001_C_Training/chapter03_Control_Flow_computations/a34_debugging_switch/src/main.c
@@ -1,42 +1,47 @@
 #include <stdio.h>
 
-int main() {
+int main()
+{
     // Pre-defined input (consider making this a parameter or array of char)
     // For simplicity, we'll use an array for direct "hard-coded" input
-    const char input[] = " Hello 123 World 42"; // Adjust to match your exact input
+    const char input[]
+        = " Hello 123 World 42"; // Adjust to match your exact input
 
     int c;
-    int nwhite = 0, nother = 0, ndigit[10] = {0}; // Initialize counts
+    int nwhite = 0, nother = 0, ndigit[10] = { 0 }; // Initialize counts
 
-    for (c = 0; input[c] != '\0'; c++) { // Use loop to process each character as if from getchar()
+    for (c = 0; input[c] != '\0';
+        c++) { // Use loop to process each character as if from getchar()
         switch (input[c]) {
-            case '0':
-            case '1':
-            case '2':
-            case '3':
-            case '4':
-            case '5':
-            case '6':
-            case '7':
-            case '8':
-            case '9': // Digits
-                ndigit[input[c] - '0']++;
-                break;
-            case ' ':
-            case '\t':
-                nwhite++; // Space or tab
-                break;
-            default: // Any other character (non-digit, non-space/tab)
-                nother++;
-                break;
+        case '0':
+        case '1':
+        case '2':
+        case '3':
+        case '4':
+        case '5':
+        case '6':
+        case '7':
+        case '8':
+        case '9': // Digits
+            ndigit[input[c] - '0']++;
+            break;
+        case ' ':
+        case '\t':
+            nwhite++; // Space or tab
+            break;
+        default: // Any other character (non-digit, non-space/tab)
+            nother++;
+            break;
         }
     }
 
     // Print results
     printf("digits =");
     for (int i = 0; i < 10; i++) {
-        if (ndigit[i]) printf(" %d", ndigit[i]);
-        else printf(" 0"); // Clean up empty digits if any
+        if (ndigit[i])
+            printf(" %d", ndigit[i]);
+        else
+            printf(" 0"); // Clean up empty digits if any
     }
     printf(", white space = %d, other = %d\n", nwhite, nother);
     printf("\nDone!\n");
