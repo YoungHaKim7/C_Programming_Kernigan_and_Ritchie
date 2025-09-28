c1bd56c0d13ec8ea506cfbf08906f83105cce4dc
GYoung <ytok1108@aol.com>
2025-09-28 18:42:49 +0900

    assignment operators

---
+++ 001_C_Training/chapter02/c01_assignment_operators/src/main.c
@@ -0,0 +1,89 @@
+#include <stdio.h>
+
+// Print an integer in binary (32-bit wide)
+void print_binary(int x)
+{
+    // for (int i = 31; i >= 0; i--) {
+    for (int i = 15; i >= 0; i--) {
+        printf("%d", (x >> i) & 1);
+        if (i % 4 == 0)
+            printf(" "); // group bits in 4s
+    }
+}
+
+int main(void)
+{
+    int a, b;
+
+    a = 20;
+    b = 10;
+
+    printf("Initial values: \n\ta = %d (", a);
+    print_binary(a);
+    printf("), \n\tb = %d (", b);
+    print_binary(b);
+    printf(")\n\n");
+
+    a = 20;
+    a += b;
+    printf("(+=)  a += b → %d (", a);
+    print_binary(a);
+    printf(")\n");
+
+    a = 20;
+    a -= b;
+    printf("(-=)  a -= b → %d (", a);
+    print_binary(a);
+    printf(")\n");
+
+    a = 20;
+    a *= b;
+    printf("(*=)  a *= b → %d (", a);
+    print_binary(a);
+    printf(")\n");
+
+    a = 20;
+    a /= b;
+    printf("(/=)  a /= b → %d (", a);
+    print_binary(a);
+    printf(")\n");
+
+    a = 20;
+    b = 7;
+    a %= b;
+    printf("(%%=)  a %%= b → %d (", a);
+    print_binary(a);
+    printf(")  a=20, b=7 \n");
+
+    a = 20;
+    a &= b;
+    printf("(&=)  a &= b → %d (", a);
+    print_binary(a);
+    printf(")\n");
+
+    a = 20;
+    a |= b;
+    printf("(|=)  a |= b → %d (", a);
+    print_binary(a);
+    printf(")\n");
+
+    a = 20;
+    a ^= b;
+    printf("(^=)  a ^= b → %d (", a);
+    print_binary(a);
+    printf(")\n");
+
+    a = 20;
+    a <<= b;
+    printf("(<<=) a <<= b → %d (", a);
+    print_binary(a);
+    printf(")\n");
+
+    a = 20;
+    a >>= b;
+    printf("(>>=) a >>= b → %d (", a);
+    print_binary(a);
+    printf(")\n");
+
+    return 0;
+}
