11af6b20a409a1d4885dfaab56dbcf21ede21459
GYoung <ytok1108@aol.com>
2025-09-30 01:38:11 +0900

    fmt

--- 001_C_Training/chapter03_Control_Flow_computations/a35_loops_and_for/src/main.c
+++ 001_C_Training/chapter03_Control_Flow_computations/a35_loops_and_for/src/main.c
@@ -1,8 +1,9 @@
-#include <stdio.h>
 #include <ctype.h>
+#include <stdio.h>
 
 // atoi: int atoi(char s[]) convert s to integer; version 2
-int atoi_v2(char s[]) {
+int atoi_v2(char s[])
+{
     int i, n = 0, sign;
     for (i = 0; isspace((unsigned char)s[i]); i++)
         ;
@@ -15,7 +16,8 @@ int atoi_v2(char s[]) {
 }
 
 // shellsort: sort v[0]...v[n-1] into increasing order
-void shellsort(int v[], int n) {
+void shellsort(int v[], int n)
+{
     int gap, i, j, temp;
     for (gap = n / 2; gap > 0; gap /= 2)
         for (i = gap; i < n; i++)
@@ -26,7 +28,8 @@ void shellsort(int v[], int n) {
             }
 }
 
-void print_array(const char *msg, int v[], int n) {
+void print_array(const char* msg, int v[], int n)
+{
     printf("%s", msg);
     for (int i = 0; i < n; i++) {
         printf("%d ", v[i]);
@@ -34,13 +37,11 @@ void print_array(const char *msg, int v[], int n) {
     printf("\n");
 }
 
-int main(void) {
+int main(void)
+{
     // --- atoi tests ---
-    char *tests[] = {
-        "123", "-456", "   789", "+42", "00099",
-        "  -0010", "2147483647", "-2147483648",
-        "  +0000123", "999999999"
-    };
+    char* tests[] = { "123", "-456", "   789", "+42", "00099", "  -0010",
+        "2147483647", "-2147483648", "  +0000123", "999999999" };
 
     printf("=== ATOI TESTS ===\n");
     for (int i = 0; i < 10; i++) {
@@ -51,27 +52,27 @@ int main(void) {
     // --- shellsort tests ---
     printf("\n=== SHELLSORT TESTS ===\n");
 
-    int arr1[] = {5, 2, 9, 1, 5, 6};
+    int arr1[] = { 5, 2, 9, 1, 5, 6 };
     int n1 = sizeof(arr1) / sizeof(arr1[0]);
     shellsort(arr1, n1);
     print_array("[arr1 sorted] ", arr1, n1);
 
-    int arr2[] = {10, 9, 8, 7, 6, 5};
+    int arr2[] = { 10, 9, 8, 7, 6, 5 };
     int n2 = sizeof(arr2) / sizeof(arr2[0]);
     shellsort(arr2, n2);
     print_array("[arr2 sorted] ", arr2, n2);
 
-    int arr3[] = {1, 2, 3, 4, 5, 6};
+    int arr3[] = { 1, 2, 3, 4, 5, 6 };
     int n3 = sizeof(arr3) / sizeof(arr3[0]);
     shellsort(arr3, n3);
     print_array("[arr3 sorted] ", arr3, n3);
 
-    int arr4[] = {42};
+    int arr4[] = { 42 };
     int n4 = sizeof(arr4) / sizeof(arr4[0]);
     shellsort(arr4, n4);
     print_array("[arr4 sorted] ", arr4, n4);
 
-    int arr5[] = {3, -1, 0, -7, 2};
+    int arr5[] = { 3, -1, 0, -7, 2 };
     int n5 = sizeof(arr5) / sizeof(arr5[0]);
     shellsort(arr5, n5);
     print_array("[arr5 sorted] ", arr5, n5);
