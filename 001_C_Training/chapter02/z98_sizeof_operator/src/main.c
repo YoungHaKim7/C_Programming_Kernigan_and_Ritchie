d732612d2ccfdebf8f803a8a12e95012dd210d9b
GYoung <ytok1108@aol.com>
2025-09-30 02:10:05 +0900

    fmt +  sizeof Operator

--- 001_C_Training/chapter02/z98_sizeof_operator/src/main.c
+++ 001_C_Training/chapter02/z98_sizeof_operator/src/main.c
@@ -2,20 +2,22 @@
 
 int main(void)
 {
+    // clang-format off
     short x;
     // type argument:
     printf("sizeof(float)          = %zu\n", sizeof(float));
     printf("sizeof(void(*)(void))  = %zu\n", sizeof(void (*)(void)));
     printf("sizeof(char[10])       = %zu\n", sizeof(char[10]));
-    //  printf("sizeof(void(void))     = %zu\n", sizeof(void(void))); // Error:
-    //  function type printf("sizeof(char[])         = %zu\n", sizeof(char[]));
-    //  // Error: incomplete type
+//  printf("sizeof(void(void))     = %zu\n", sizeof(void(void))); // Error: function type
+//  printf("sizeof(char[])         = %zu\n", sizeof(char[])); // Error: incomplete type
 
+    //
     // expression argument:
     printf("sizeof 'a'             = %zu\n", sizeof 'a'); // type of 'a' is int
-    //  printf("sizeof main            = %zu\n", sizeof main); // Error:
-    //  Function type
+//  printf("sizeof main            = %zu\n", sizeof main); // Error: Function type
     printf("sizeof &main           = %zu\n", sizeof &main);
+    // clang-format on
+    //
     printf(
         "sizeof \"hello\"         = %zu\n", sizeof "hello"); // type is char[6]
     printf("sizeof x               = %zu\n", sizeof x); // type of x is short
