382382898e53012349c2eec16b28e730abef17ea
GYoung <ytok1108@aol.com>
2025-10-03 20:18:45 +0900

    reverse + fmt

--- 001_C_Training/chapter03_Control_Flow_computations/a35_04_loops_for_reverse/src/main.c
+++ 001_C_Training/chapter03_Control_Flow_computations/a35_04_loops_for_reverse/src/main.c
@@ -1,12 +1,12 @@
 #include <stdio.h>
 #include <string.h>
 
-void reverse(char s[]) {
+void reverse(char s[])
+{
     int c, i, j;
 
-    for (i = 0, j = strlen(s) -1; i < j; i++, j--)
-    {
-        c = s [i];
+    for (i = 0, j = strlen(s) - 1; i < j; i++, j--) {
+        c = s[i];
         s[i] = s[j];
         s[j] = c;
     }
@@ -15,11 +15,11 @@ void reverse(char s[]) {
 int main(void)
 {
     char test_string[] = "Hello, World!";
-    
+
     printf("Original string: %s\n", test_string);
     reverse(test_string);
     printf("Reversed string: %s\n", test_string);
-    
+
     printf("\nDone!\n");
     return 0;
 }
