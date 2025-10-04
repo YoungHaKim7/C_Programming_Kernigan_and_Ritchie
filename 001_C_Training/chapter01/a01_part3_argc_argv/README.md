794828dca8370da2c3f26de08cf97c1726a860fc
GYoung <ytok1108@aol.com>
2025-10-04 17:57:42 +0900

    main argc argv test

---
+++ 001_C_Training/chapter01/a01_part3_argc_argv/README.md
@@ -0,0 +1,10 @@
+# Result
+
+- macOS
+
+```bash
+$ ./target/a01_part3_argc_argv test_GYOUNg
+
+Hello world C lang ./target/a01_part3_argc_argvtest_GYOUNg⏎ 
+
+```
---
+++ 001_C_Training/chapter01/a01_part3_argc_argv/src/main.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+int main(int argc, char* argv[]) {
+    printf("Hello world C lang ");
+    int i;
+    
+    for (i=0; i < argc; i++) {
+        printf("%s", argv[i]);
+    }
+    return 0;
+}
