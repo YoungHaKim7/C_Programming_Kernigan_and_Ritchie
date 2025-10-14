5216cff3773f7e6f111811770ae1732b8d1968f4
GYoung <ytok1108@aol.com>
2025-10-15 02:12:32 +0900

    Linux OS cpp23

---
+++ 005_snippets_Cpp_code/001_Cpp23_Snippets/a01_macos_cpp23_import_std/src/main.cpp
@@ -0,0 +1,7 @@
+import std_import;
+
+int main()
+{
+    std::println("c23 hello world (import ex)");
+    return 0;
+}
---
+++ 005_snippets_Cpp_code/001_Cpp23_Snippets/a02_linuxos_cpp23_import_std/src/main.cpp
@@ -0,0 +1,7 @@
+import std_import;
+
+int main()
+{
+    println("c23 hello world (import ex)");
+    return 0;
+}
